8beb9970fc0d0460c4aaa333c309f7a7e7555147
Jonatas Melo <jonnmello@hotmail.com>
2021-12-04 18:11:57 -0300

    insertShouldInsertResource OK

--- bds02-main/src/main/java/com/devsuperior/bds02/services/CityService.java
+++ bds02-main/src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.entities.City;
@@ -22,6 +23,15 @@ public class CityService {
 		List<City> list = repository.findAll(Sort.by("name"));
 		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
 				
+	}
+	
+	@Transactional
+	public CityDTO insert(CityDTO dto) {
+		City entity = new City();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CityDTO(entity);
+		
 	}
 
 }
